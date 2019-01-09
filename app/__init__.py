8b2bd0f40533e36d9bc0fc5146092370acc80bca
Barasa <emmanuelbarasa5@gmail.com>
2019-01-09 12:38:26 +0300

    [modifies #163080128]set up directory structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-from instance.config import app_config
-
 def create_app():
     """
     Create app
@@ -9,5 +7,4 @@ def create_app():
   
     # app initiliazation
     app = Flask(__name__)
-    app.config.from_object(app_config[config])
     return app
